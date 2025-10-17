641c9c27afc2449325064260a7ac24e91209023d
CelineYay <celinesutdnotes@gmail.com>
2025-10-18 01:08:16 +0800

    ngrok working

--- PPCatCafe.py
+++ PPCatCafe.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from pyngrok import ngrok
 import ppccGlobals as ppc
-
+'''
 ngrok.set_auth_token("") #add your own auth token here
 # Start ngrok tunnel to expose the Streamlit server
 ngrok_tunnel = ngrok.connect(addr='5011', proto='http', bind_tls=True) #5011
 # Print the URL of the ngrok tunnel
 print(' * Tunnel URL:', ngrok_tunnel.public_url)
 # to get ngrok to run, use "streamlit run PPCatCafe.py --server.port 5011"
+'''
 
 st.title("Welcome to our Cafe! 🐱☕️🍰")
 st.write("Please pay for entry to hangout with the cats")
